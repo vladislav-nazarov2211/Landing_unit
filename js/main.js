e9957d0cc0bece469b9e3a2809801bbdb3707723
Vlad <vald.nazarov95@yandex.ru>
2022-11-06 13:37:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        // loop:true,
+        margin:70,
+        items: 1,
+        nav: true,
+        navText: ['', '']
+    });
+});
